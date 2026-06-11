77826d9ea44d7bf483f2cda57dc344797ec14a15
affasaleem <affasaleem.it@gmail.com>
2026-06-12 03:37:40 +0530

    Files modified

--- JavaScript Practicals/Section-16/DOM+Challenge+Starting+Files/DOM Challenge Starting Files/index.js
+++ JavaScript Practicals/Section-16/DOM+Challenge+Starting+Files/DOM Challenge Starting Files/index.js
@@ -1,1 +1,8 @@
-document.query
+
+
+// document.querySelector("h1").innerHTML = "good bye!";
+
+document.firstElementChild.lastElementChild.querySelector("ul").lastElementChild.innerHTML = "Affa";
+
+
+//did the practices in INSPECT
